f602d42e1deeab2ddba0917f4451c13f07a89a22
hruthika9 <hruthikaeedupuganti20@gmail.com>
2021-12-16 12:14:36 +0530

    m

--- app.js
+++ app.js
@@ -3,17 +3,16 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.set('views', __dirname + '/views');
+//app.set('uploads', __dirname + '/uploads');
 
-//app.use(express.static("public"));
 app.use(express.static(__dirname + '/public'));
 
 app.use(require('./routes/list'));
 app.use(require('./routes/index'));
-
 app.use(require('./routes/register'));
 app.use(require('./routes/creator-login'));
 app.use(require('./routes/upload'));
 
-app.listen(5000, process.env.HOST || '127.0.0.1', () => {
+app.listen(5000, '0.0.0.0', () => {
     console.log("server up and running");
 });
